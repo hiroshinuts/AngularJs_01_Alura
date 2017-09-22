1e85ae5eaf3ff6a36827c1465f699b4eab364533
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2017-09-22 15:14:29 -0300

    FIM CAP 9

--- public/js/main.js
+++ public/js/main.js
@@ -12,6 +12,11 @@ angular.module('alurapic', ['minhasDiretivas' , 'ngAnimate', 'ngRoute'])
         templateUrl: 'partials/foto.html',
         controller: 'FotoController'
     });
+
+    $routeProvider.when('/fotos/edit/:fotoId', {
+        templateUrl: 'partials/foto.html',
+        controller: 'FotoController'
+    });
     
 
     $routeProvider.otherwise({redirectTo: '/fotos'});
